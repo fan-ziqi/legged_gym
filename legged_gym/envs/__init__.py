99dbcc4b19f6b34c08f8ef19ad66c2eeb8db52b8
fan-ziqi <fanziqi614@gmail.com>
2023-07-06 15:10:18 +0800

    feat: add some robots

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -37,7 +37,10 @@ from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPP
 from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
-from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
+from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO, A1FlatCfg, A1FlatCfgPPO
+from .cyberdog.cyberdog_config import CyberdogRoughCfg, CyberdogRoughCfgPPO, CyberdogFlatCfg, CyberdogFlatCfgPPO, CyberdogMeihuaCfg, CyberdogMeihuaCfgPPO
+from .keti1.keti1_config import Keti1RoughCfg, Keti1RoughCfgPPO, Keti1FlatCfg, Keti1FlatCfgPPO
+from .go1.go1_config import Go1RoughCfg, Go1RoughCfgPPO, Go1FlatCfg, Go1MrssNovel, Go1FlatCfgPPO, Go1FlatNoVelCfg, Go1FlatNoVelCfgPPO, Go1MrssNovelRough
 
 
 import os
@@ -47,5 +50,16 @@ from legged_gym.utils.task_registry import task_registry
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
 task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
-task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
+task_registry.register( "a1_rough", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
+task_registry.register( "a1_flat", LeggedRobot, A1FlatCfg(), A1FlatCfgPPO())
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+task_registry.register( "cyberdog_rough", LeggedRobot, CyberdogRoughCfg(), CyberdogRoughCfgPPO() )
+task_registry.register( "cyberdog_flat", LeggedRobot, CyberdogFlatCfg(), CyberdogFlatCfgPPO() )
+task_registry.register( "cyberdog_meihua", LeggedRobot, CyberdogMeihuaCfg(), CyberdogMeihuaCfgPPO() )
+task_registry.register( "keti1_rough", LeggedRobot, Keti1RoughCfg(), Keti1RoughCfgPPO() )
+task_registry.register( "keti1_flat", LeggedRobot, Keti1FlatCfg(), Keti1FlatCfgPPO() )
+task_registry.register("go1_rough", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO())
+task_registry.register("go1_flat", LeggedRobot, Go1FlatCfg(), Go1FlatCfgPPO())
+task_registry.register("go1_flat_novel", LeggedRobot, Go1FlatNoVelCfg(), Go1FlatNoVelCfgPPO())
+task_registry.register("go1_mrss_novel", LeggedRobot, Go1MrssNovel(), Go1FlatNoVelCfgPPO())
+task_registry.register("go1_mrss_novel_rough", LeggedRobot, Go1MrssNovelRough(), Go1FlatNoVelCfgPPO())
