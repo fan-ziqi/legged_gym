539fa6fa8559d4a141bbd45165266c373b924b51
fan-ziqi <fanziqi614@gmail.com>
2023-09-07 20:01:54 +0800

    feat: add AMP envs

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -30,7 +30,8 @@
 
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .base.legged_robot import LeggedRobot
+from .base.legged_robot import LeggedRobot as LeggedRobot
+from .base.legged_robot_amp import LeggedRobot as LeggedRobotAMP
 from .anymal_c.anymal import Anymal
 from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
 from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPPO
@@ -38,8 +39,10 @@ from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO, A1FlatCfg, A1FlatCfgPPO
+from .a1.a1_amp_config import A1AMPCfg, A1AMPCfgPPO
 from .cyberdog.cyberdog_config import CyberdogRoughCfg, CyberdogRoughCfgPPO, CyberdogFlatCfg, CyberdogFlatCfgPPO, CyberdogMeihuaCfg, CyberdogMeihuaCfgPPO
 # from .keti1.keti1_config import Keti1RoughCfg, Keti1RoughCfgPPO, Keti1FlatCfg, Keti1FlatCfgPPO
+# from .keti1.keti1_amp_config import Keti1AMPCfg, Keti1AMPCfgPPO
 # from .go1.go1_config import Go1RoughCfg, Go1RoughCfgPPO, Go1FlatCfg, Go1MrssNovel, Go1FlatCfgPPO, Go1FlatNoVelCfg, Go1FlatNoVelCfgPPO, Go1MrssNovelRough
 
 
@@ -52,12 +55,14 @@ task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCf
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1_rough", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "a1_flat", LeggedRobot, A1FlatCfg(), A1FlatCfgPPO())
+task_registry.register( "a1_amp", LeggedRobotAMP, A1AMPCfg(), A1AMPCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
 task_registry.register( "cyberdog_rough", LeggedRobot, CyberdogRoughCfg(), CyberdogRoughCfgPPO() )
 task_registry.register( "cyberdog_flat", LeggedRobot, CyberdogFlatCfg(), CyberdogFlatCfgPPO() )
 task_registry.register( "cyberdog_meihua", LeggedRobot, CyberdogMeihuaCfg(), CyberdogMeihuaCfgPPO() )
 # task_registry.register( "keti1_rough", LeggedRobot, Keti1RoughCfg(), Keti1RoughCfgPPO() )
 # task_registry.register( "keti1_flat", LeggedRobot, Keti1FlatCfg(), Keti1FlatCfgPPO() )
+# task_registry.register( "keti1_amp", LeggedRobotAMP, Keti1AMPCfg(), Keti1AMPCfgPPO() )
 # task_registry.register("go1_rough", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO())
 # task_registry.register("go1_flat", LeggedRobot, Go1FlatCfg(), Go1FlatCfgPPO())
 # task_registry.register("go1_flat_novel", LeggedRobot, Go1FlatNoVelCfg(), Go1FlatNoVelCfgPPO())
