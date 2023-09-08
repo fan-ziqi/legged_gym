4a59fd1d71da2863c3b6a6085efef92841536d45
fan-ziqi <fanziqi614@gmail.com>
2023-09-08 11:25:47 +0800

    fix: update cyberdog2 env config

--- legged_gym/envs/cyberdog2/cyberdog2_config.py
+++ legged_gym/envs/cyberdog2/cyberdog2_config.py
@@ -9,30 +9,30 @@ class Cyberdog2RoughCfg( LeggedRobotCfg ):
         mesh_type = 'trimesh'
         border_size = 15 # [m] 边界大小
         # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete, flat]
-        terrain_proportions = [0.2, 0.2, 0.2, 0.2, 0.2, 0.0]
+        terrain_proportions = [0.1, 0.1, 0.3, 0.3, 0.2]
         # terrain_proportions = [0.0, 0.0, 1.0, 0.0, 0.0, 0.0] # 上楼梯
         num_rows = 10 # number of terrain rows (levels)
-        num_cols = 20 # number of terrain cols (types)
+        num_cols = 10 # number of terrain cols (types)
         # measured_points_x = [-1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] # 测量点的x位置 1.4mx2.0m rectangle (without center line)
         # measured_points_y = [-0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7] # 测量点的y位置
 
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.40] # x,y,z [m]
+        pos = [0.0, 0.0, 0.30] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
             'FL_hip_joint': 0.0,   # [rad]
             'RL_hip_joint': 0.0,   # [rad]
             'FR_hip_joint': -0.0 ,  # [rad]
             'RR_hip_joint': -0.0,   # [rad]
 
-            'FL_thigh_joint': 0.8,     # [rad]
-            'RL_thigh_joint': 1.0,   # [rad]
-            'FR_thigh_joint': 0.8,     # [rad]
-            'RR_thigh_joint': 1.0,   # [rad]
+            'FL_thigh_joint': 0.66,     # [rad]
+            'RL_thigh_joint': 0.66,   # [rad]
+            'FR_thigh_joint': 0.66,     # [rad]
+            'RR_thigh_joint': 0.66,   # [rad]
 
-            'FL_calf_joint': -1.5,   # [rad]
-            'RL_calf_joint': -1.5,    # [rad]
-            'FR_calf_joint': -1.5,  # [rad]
-            'RR_calf_joint': -1.5,    # [rad]
+            'FL_calf_joint': -1.17,   # [rad]
+            'RL_calf_joint': -1.17,    # [rad]
+            'FR_calf_joint': -1.17,  # [rad]
+            'RR_calf_joint': -1.17,    # [rad]
         }
 
     class control( LeggedRobotCfg.control ):
@@ -50,13 +50,13 @@ class Cyberdog2RoughCfg( LeggedRobotCfg ):
         name = "cyberdog2"
         foot_name = "foot"
         penalize_contacts_on = ["thigh", "calf"]
-        terminate_after_contacts_on = ["base", "hip"]
-        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
         # flip_visual_attachments = False
 
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
-        base_height_target = 0.3
+        base_height_target = 0.25
         class scales( LeggedRobotCfg.rewards.scales ):
             torques = -0.0002
             feet_air_time =  1.0
@@ -127,7 +127,7 @@ class Cyberdog2MeihuaCfg( Cyberdog2RoughCfg ):
   
     class rewards( Cyberdog2RoughCfg.rewards ):
         soft_dof_pos_limit = 0.9
-        base_height_target = 0.30
+        base_height_target = 0.25
         class scales( Cyberdog2RoughCfg.rewards.scales ):
             torques = -0.0002
             feet_air_time =  2.0
